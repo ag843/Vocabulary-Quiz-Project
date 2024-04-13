f3498cea3cd81504cdd3452fb7de9cfeb315c91c
Ashna Gupta <148406858+ashnagupta843@users.noreply.github.com>
2024-04-13 00:21:18 -0400

    Update readVocabData.m

--- readVocabData.m
+++ readVocabData.m
@@ -1,36 +1,50 @@
 function [vocabData, uRating] = readVocabData(inFilename)
-% Read input data file and return the vocabulary data and user rating.
-% inFilename is the name of the input data file to be read:
-%   * First line of data file:  the number of words in the vocabulary,
-%     followed by the user rating for this vocabulary.
-%   * Each remaining line is the data for one word in the vocabulary; there
-%     are four data items on each line:  the average time-to-answer (in 
-%     tenths of a second), the rating of the word, the word, the word's 
-%     definition.
-%   * Within each line, data items are delimited by tabs.
-% vocabData is a 2-d cell array; its number of rows is the number of words 
-% in the vocabulary and its number of columns is 4. Each row stores the  
-% data for one word in the vocabulary:
-%   * The first column of any particular row stores a word
-%   * The second column of that row stores the word's definition
-%   * The third column of that row stores the average time-to-answer (in 
-%     tenths of a second)
-%   * The fourth column of that row stores the difficulty rating of the 
-%     word
-% uRating is the user rating read from line 1 of the data file.
-fid=fopen(inFilename,'r');
-L= fgetl(fid);
-info= textscan(L,'%f %f','delimiter','\t');
-numWords=info{1};
-vocabData = cell(numWords,4);
-uRating=info{2};
-for i=1:1:numWords
-    L= fgetl(fid); % read a line from the file with identifier fid; L is then a char vector
-    info= textscan(L,'%f %f %s %s','delimiter', '\t');
-    ca=cellstr2str(info);
-    vocabData{i,3}= ca{1}; % t stores the type double vector in cell 1 (the scalar 200 in this case)
-    vocabData{i,4}= ca{2}; % r stores the type double vector in cell 2 (the scalar 1000 in this case)
-    vocabData{i,1}= ca{3}; % w stores the char vector 3aberrant1
-    vocabData{i,2}= ca{4}; % d stores the char vector 4deviating from normal or correct2  
-end 
-fclose(fid);
+%READVOCABDATA Read vocabulary data and user rating from a file.
+%
+%   [vocabData, uRating] = READVOCABDATA(inFilename) reads vocabulary data
+%   from the file specified by inFilename. The file's first line contains
+%   the number of words and user rating. Each subsequent line contains data
+%   for one word.
+%
+%   inFilename is the name of the input data file.
+%   vocabData is a cell array with each row containing data for one word:
+%     1. Word
+%     2. Definition
+%     3. Time-to-answer (tenths of a second)
+%     4. Difficulty rating
+%   uRating is the user rating for the vocabulary.
+
+    % Open the file for reading
+    fid = fopen(inFilename, 'r');
+    if fid == -1
+        error('Failed to open file %s for reading.', inFilename);
+    end
+    
+    try
+        % Read the first line for number of words and user rating
+        firstLine = fgetl(fid);
+        info = textscan(firstLine, '%f %f', 'Delimiter', '\t');
+        numWords = info{1};
+        uRating = info{2};
+
+        % Initialize vocabData
+        vocabData = cell(numWords, 4);
+
+        % Read each word's data
+        for i = 1:numWords
+            line = fgetl(fid);
+            data = textscan(line, '%f %f %s %s', 'Delimiter', '\t');
+            vocabData{i, 3} = data{1}; % Average time-to-answer
+            vocabData{i, 4} = data{2}; % Difficulty rating
+            vocabData{i, 1} = data{3}{1}; % Word
+            vocabData{i, 2} = data{4}{1}; % Definition
+        end
+    catch err
+        % Close file on error and rethrow the error
+        fclose(fid);
+        rethrow(err);
+    end
+
+    % Close the file after reading
+    fclose(fid);
+end
